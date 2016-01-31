3ec65813df08b4b658fea26f653b19df9f4d2c2e
Ryan <rwsargent@yahoo.com>
2016-01-30 21:33:32 -0700

    change

--- scripts/tourism.js
+++ scripts/tourism.js
@@ -1,30 +1,30 @@
 module.exports = [
     {
-	title: 'Vancouver Aquarium',
-	link : 'http://www.vanaqua.org/',
-	img : '/images/vancouver_aqua_logo.jpeg',
-	blurb : "The Vancouver Aquarium is a non-profit society dedicated to the conservation of aquatic life, and a favorite of Ella and her brother any trip to Vancouver.",
-	cssId : 'aqua'
-    }, {
 	title: 'Lighthouse Park in West Vancouver',
 	link : 'http://www.lighthousepark.ca/',
 	img : '/images/lighthouse.jpg',
-	blurb : 'One of our favorte places to take a stroll! Lighthouse Park located in West Vancouver British Columbia is one of the most amazing scenic parks you will see on the West Coast. Walk through acres of first-growth douglas firs and stand next to trees that are considered to be giants.',
+	blurb : 'One of our favourite places to take a stroll! Lighthouse Park, located in West Vancouver, is one of the most amazing scenic parks you will see on the West Coast. Walk through acres of first-growth douglas firs and stand next to trees that are considered to be giants.',
 	cssId : '',
 	jeff : true
-    }, {
-	title: 'Museum of Anthropology',
-	link : 'http://moa.ubc.ca',
-	img : '/images/MOA_Profile_Google_500.png',
-	blurb : "The Museum of Anthropology at the University of British Columbia campus in Vancouver, British Columbia, Canada is renowned for its displays of world arts and cultures, in particular works by First Nation peoples of the Pacific Northwest. The magnificent entryway doors are from Ella's ancestral home.",
+    },{
+	title: 'Vancouver Aquarium',
+	link : 'http://www.vanaqua.org/',
+	img : '/images/vancouver_aqua_logo.jpeg',
+	blurb : "The Vancouver Aquarium is a non-profit society dedicated to the conservation of aquatic life, and a childhood favourite during Ella's visits to Vancouver.",
 	cssId : 'aqua'
-    }, {
+    },{
 	title: 'Seawall in Vancouver',
 	link : 'http://vancouver.ca/parks-recreation-culture/seawall.aspx',
 	img : '/images/seawallbikes.jpg',
-	blurb : "We love to ride our bikes or walk up and down the Seawall. It's the world's longest uninterrupted waterfront path.",
+	blurb : "We love to ride our bikes up and down the Seawall, or just enjoy a nice leisurely stroll. It's the world's longest uninterrupted waterfront path.",
 	cssId : '',
 	jeff : true
+    },{
+	title: 'Museum of Anthropology',
+	link : 'http://moa.ubc.ca',
+	img : '/images/MOA_Profile_Google_500.png',
+	blurb : "The Museum of Anthropology at the University of British Columbia campus is renowned for its displays of world arts and cultures, in particular, works by First Nation peoples of the Pacific Northwest. The magnificent entryway doors are from Ella's ancestral home.",
+	cssId : 'aqua'
     },{
 	title: 'Granville Island',
 	link : 'http://granvilleisland.com/',
@@ -43,14 +43,14 @@ module.exports = [
 	title: 'Telus Wolrd of Science',
 	link : 'http://scienceworld.ca/',
 	img : '/images/telus_science.JPG',
-	blurb : "Science World great place to go with kids because its fun for adults too! There are a lot of cool exhibits and interactive displays.",
+	blurb : "Science World is a great place to go with kids because it is fun for adults too! There are a lot of cool exhibits and interactive displays.",
 	cssId : '',
 	jeff : true
     },{
 	title: 'Kitsilano Beanch',
 	link : 'http://vancouver.ca/parks-recreation-culture/kitsilano-beach.aspx',
 	img : '/images/kits_beach.JPG',
-	blurb : "Our favourite summer past time when not hiking through the mountains is just heading to the beach.  This also may have been where a certain someone proposed...",
+	blurb : "Our favourite summer pastime (when not hiking through the mountains) is just heading to the beach.  This also may have been where a certain someone proposed...",
 	cssId : '',
 	jeff : true
     }
